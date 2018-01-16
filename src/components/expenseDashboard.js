b2775a6048f7b0d169f9eb50ca776c6c2db74614
Jordan <jordanhuri96@gmail.com>
2018-01-16 02:47:51 +0200

    fixed a minor bug

--- src/components/expenseDashboard.js
+++ src/components/expenseDashboard.js
@@ -5,12 +5,13 @@ import ExpenseListFilters from './expenseListFilters';
 import selectExpenses from '../selectors/expenses';
 import expensesTotal from '../selectors/expensesTotal';
 import {Link} from 'react-router-dom';
+import numeral from 'numeral';
 
 const ExpenseDashboard = (props) => (
     <div>
         <div className="page-header">
             <div className="content-container">
-                <h1 className="page-header__title">Viewing <span>{props.expenses.length}</span> expenses with a sum of <span>${props.total > 0 ? props.total : 0}</span></h1>
+                <h1 className="page-header__title">Viewing <span>{props.expenses.length}</span> expenses with a sum of <span>${props.total > 0 ? numeral(props.total).format('0,0.00') : '0.00'}</span></h1>
                 <div className='page-header__actions'>
                     <Link className='button' to='/create'>Add Expense</Link>
                 </div>
